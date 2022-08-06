e056c7ffca8ef4acf20b8334d10a06716dc7b6ce
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-08-06 22:08:14 +0200

    update

--- frontend/components/subComponents/logo/Logo.jsx
+++ frontend/components/subComponents/logo/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo() {
     <div className='flex items-center cursor-pointer'>
       <img className='w-14' src="/images/dice-img.png" alt='award image' />
         {/*  <img className='w-14' src="/images/dice-img.png" alt="award image" /> */}
-        <p className='text-2xl px-3 font-bold text-blue-500'>Get Lucky 33</p>
+        <p className='text-2xl px-3 font-bold text-yellow-500'>Get Lucky 33</p>
     </div>
   )
 }
